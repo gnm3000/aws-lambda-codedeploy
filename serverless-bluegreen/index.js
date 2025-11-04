b12232affc480a85180be7a7283b9fb7b4e22ff0
German Montenegro <gnm3000@gmail.com>
2025-11-03 21:00:17 -0300

    first blue green commit

--- serverless-bluegreen/index.js
+++ serverless-bluegreen/index.js
@@ -1,8 +1,10 @@
-exports.handler = async () => {
+// index.js
+exports.handler = async (event) => {
+  throw new Error("Forced failure for rollback tests");
   return {
     statusCode: 200,
     body: JSON.stringify({
-      message: "Hello from Blue/Green Lambda v1"
+      message: "Hello from Blue/Green Lambda (Node 20, CommonJS- ok-v5)",
     }),
   };
 };
